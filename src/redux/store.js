19b2fa9339a61242ce9ade10cfdd454c8c25e075
TOR10S <andriivoro@gmail.com>
2024-10-16 14:30:54 +0300

    fixed 1

--- src/redux/store.js
+++ src/redux/store.js
@@ -23,7 +23,7 @@ const authPersistConfig = {
 export const store = configureStore({
   reducer: {
     auth: persistReducer(authPersistConfig, authReducer),
-    tasks: ContactsReducer,
+    contacts: ContactsReducer,
   },
   middleware: (getDefaultMiddleware) =>
     getDefaultMiddleware({
